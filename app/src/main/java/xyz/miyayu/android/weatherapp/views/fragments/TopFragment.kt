9cca396d5b915e5e9ac7b52600441fc9449d5b2c
Miyayu <yuchan2215@gmail.com>
2022-08-01 12:41:20 +0900

    [add] #9 天気の一覧画面を作成

--- app/src/main/java/xyz/miyayu/android/weatherapp/views/fragments/TopFragment.kt
+++ app/src/main/java/xyz/miyayu/android/weatherapp/views/fragments/TopFragment.kt
@@ -5,14 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import xyz.miyayu.android.weatherapp.databinding.TopFragmentBinding
+import xyz.miyayu.android.weatherapp.utils.ViewModelFactories
+import xyz.miyayu.android.weatherapp.viewmodel.SettingViewModel
+import xyz.miyayu.android.weatherapp.views.adapters.AreasListAdapter
 
 /**
  * トップ画面のフラグメント
  */
 class TopFragment : Fragment() {
     private lateinit var binding: TopFragmentBinding
+    private lateinit var viewModel: SettingViewModel
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val viewModelFactory = ViewModelFactories.getSettingViewModelFactory()
+        viewModel = ViewModelProvider(this, viewModelFactory)[SettingViewModel::class.java]
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,6 +45,22 @@ class TopFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.settingButton.setOnClickListener {
             view.findNavController().navigate(TopFragmentDirections.openSetting())
+        }
+
+        val listAdapter = AreasListAdapter {}
+
+        binding.areaRecyclerView.apply {
+            adapter = listAdapter
+            layoutManager = LinearLayoutManager(this@TopFragment.context)
+            //項目ごとに区切り線を入れる。
+            addItemDecoration(
+                DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL)
+            )
+        }
+        viewModel.areaList.observe(viewLifecycleOwner) { items ->
+            items.let {
+                listAdapter.submitList(it)
+            }
         }
     }
 }
