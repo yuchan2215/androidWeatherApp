3c68b65f76c6994f80d7239216464bc614dae49a
Miyayu <yuchan2215@gmail.com>
2022-08-01 19:28:46 +0900

    [add] 戻るを無効化できるように

--- app/src/main/java/xyz/miyayu/android/weatherapp/activities/MainActivity.kt
+++ app/src/main/java/xyz/miyayu/android/weatherapp/activities/MainActivity.kt
@@ -3,4 +3,14 @@ package xyz.miyayu.android.weatherapp.activities
 import androidx.appcompat.app.AppCompatActivity
 import xyz.miyayu.android.weatherapp.R
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    override fun onBackPressed() {
+        if (canBack) {
+            super.onBackPressed()
+        }
+    }
+
+    companion object {
+        var canBack: Boolean = true
+    }
+}
