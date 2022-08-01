6878c99db53f5998300c42fe2921040091326ed3
Miyayu <yuchan2215@gmail.com>
2022-08-01 11:51:45 +0900

    [del] 古いViewModelを削除

--- app/src/main/java/xyz/miyayu/android/weatherapp/views/fragments/settings/AreasListFragment.kt
+++ app/src/main/java/xyz/miyayu/android/weatherapp/views/fragments/settings/AreasListFragment.kt
@@ -10,8 +10,8 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import xyz.miyayu.android.weatherapp.WeatherApplication
 import xyz.miyayu.android.weatherapp.databinding.AreaListFragmentBinding
-import xyz.miyayu.android.weatherapp.viewmodel.AreasListViewModel
-import xyz.miyayu.android.weatherapp.viewmodel.AreasListViewModelFactory
+import xyz.miyayu.android.weatherapp.viewmodel.SettingViewModel
+import xyz.miyayu.android.weatherapp.viewmodel.SettingViewModelFactory
 import xyz.miyayu.android.weatherapp.views.adapters.AreasListAdapter
 
 /**
@@ -21,14 +21,15 @@ import xyz.miyayu.android.weatherapp.views.adapters.AreasListAdapter
 class AreasListFragment : Fragment() {
 
     private lateinit var binding: AreaListFragmentBinding
-    private lateinit var viewModel: AreasListViewModel
+    private lateinit var viewModel: SettingViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val dataSource = (activity?.application as WeatherApplication).database.areaDao()
-        val viewModelFactory = AreasListViewModelFactory(dataSource)
-        viewModel = ViewModelProvider(this, viewModelFactory)[AreasListViewModel::class.java]
+        val areaSource = (activity?.application as WeatherApplication).database.areaDao()
+        val settingSource = (activity?.application as WeatherApplication).database.settingDao()
+        val viewModelFactory = SettingViewModelFactory(settingSource, areaSource)
+        viewModel = ViewModelProvider(this, viewModelFactory)[SettingViewModel::class.java]
     }
 
     override fun onCreateView(
@@ -65,7 +66,7 @@ class AreasListFragment : Fragment() {
 
         }
         //地域が更新されたらリストを更新する。
-        viewModel.allAreas.observe(this.viewLifecycleOwner) { items ->
+        viewModel.areaList.observe(this.viewLifecycleOwner) { items ->
             items.let {
                 listAdapter.submitList(it)
             }
