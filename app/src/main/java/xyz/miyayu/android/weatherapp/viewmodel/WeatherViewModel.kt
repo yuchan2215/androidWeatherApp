0f56f42f186403908702a2453c414386e6f3d00d
Miyayu <yuchan2215@gmail.com>
2022-08-02 15:52:27 +0900

    [change] responseを利用した処理を作成

--- app/src/main/java/xyz/miyayu/android/weatherapp/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/xyz/miyayu/android/weatherapp/viewmodel/WeatherViewModel.kt
@@ -1,88 +1,27 @@
 package xyz.miyayu.android.weatherapp.viewmodel
 
-import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
-import xyz.miyayu.android.weatherapp.R
-import xyz.miyayu.android.weatherapp.WeatherApplication
 import xyz.miyayu.android.weatherapp.model.dao.SettingDao
 import xyz.miyayu.android.weatherapp.network.json.Weather
 import xyz.miyayu.android.weatherapp.repositories.WeatherRepository
+import xyz.miyayu.android.weatherapp.utils.Response
 
 class WeatherViewModel(val settingDao: SettingDao) : ViewModel() {
 
-    private val _status = MutableLiveData<WeatherApiStatus>(WeatherApiStatus.NONE)
-    val status: LiveData<WeatherApiStatus> = _status
+    private val _status = MutableLiveData<Response<Weather>>()
+    val status: LiveData<Response<Weather>> = _status
 
-    private val _weather = MutableLiveData<Weather>()
-    val weather: LiveData<Weather> = _weather
 
-    private fun setApiStatus(status: WeatherApiStatus) {
-        _status.value = status
-    }
+    fun fetchWeather(areaName: String) {
 
-    private fun setWeather(weather: Weather) {
-        _weather.value = weather
-    }
-
-    fun updateWeather(areaName: String) {
-        setApiStatus(WeatherApiStatus.LOADING)
+        _status.value = Response.Loading()
+        //サーバー上から天気を取得してくる
         viewModelScope.launch {
-            try {
-                val requestResponse = WeatherRepository.getWeather(areaName)
-                if (requestResponse.isSuccessful && requestResponse.body() != null) {
-                    setWeather(requestResponse.body()!!)
-                    setApiStatus(WeatherApiStatus.DONE)
-                } else {
-                    val status = when (requestResponse.raw().code) {
-                        401 -> WeatherApiStatus.UNAUTHORIZED
-                        404 -> WeatherApiStatus.NOT_FOUND
-                        else -> WeatherApiStatus.ERROR
-                    }
-                    setApiStatus(status)
-                }
-            } catch (e: Exception) {
-                e.printStackTrace()
-                setApiStatus(WeatherApiStatus.NETWORK_ERROR)
-            }
-        }
-    }
-
-
-    companion object {
-        enum class WeatherApiStatus(
-            val loadingVisibility: Int = View.GONE,
-            val resultVisibility: Int = View.GONE,
-            val errorVisibility: Int = View.GONE,
-            val errorNotFoundVisibility: Int = View.GONE,
-            val errorMessage: String = ""
-        ) {
-            NONE,
-            LOADING(loadingVisibility = View.VISIBLE),
-            DONE(resultVisibility = View.VISIBLE),
-            ERROR(
-                errorVisibility = View.VISIBLE,
-                errorMessage = WeatherApplication.instance.getString(R.string.error)
-            ),
-            APIKEY_NOT_SET(
-                errorVisibility = View.VISIBLE,
-                errorMessage = WeatherApplication.instance.getString(R.string.error_api_key_not_found)
-            ),
-            UNAUTHORIZED(
-                errorVisibility = View.VISIBLE,
-                errorMessage = WeatherApplication.instance.getString(R.string.error_unauthorized)
-            ),
-            NOT_FOUND(
-                errorNotFoundVisibility = View.VISIBLE,
-                errorMessage = WeatherApplication.instance.getString(R.string.error_not_found)
-            ),
-            NETWORK_ERROR(
-                errorVisibility = View.VISIBLE,
-                errorMessage = WeatherApplication.instance.getString(R.string.error)
-            )
+            _status.value = WeatherRepository.fetchWeather(areaName)
         }
     }
 }
--- app/src/main/java/xyz/miyayu/android/weatherapp/views/fragments/WeatherResultFragment.kt
+++ app/src/main/java/xyz/miyayu/android/weatherapp/views/fragments/WeatherResultFragment.kt
@@ -9,6 +9,9 @@ import androidx.navigation.fragment.navArgs
 import xyz.miyayu.android.weatherapp.R
 import xyz.miyayu.android.weatherapp.databinding.WeatherResultFragmentBinding
 import xyz.miyayu.android.weatherapp.model.entity.Area
+import xyz.miyayu.android.weatherapp.network.json.Weather
+import xyz.miyayu.android.weatherapp.utils.ErrorStatus
+import xyz.miyayu.android.weatherapp.utils.Response
 import xyz.miyayu.android.weatherapp.utils.ViewModelFactories
 import xyz.miyayu.android.weatherapp.viewmodel.WeatherViewModel
 import xyz.miyayu.android.weatherapp.views.fragments.settings.areas.DeleteAreaDialogFragment
@@ -42,18 +45,61 @@ class WeatherResultFragment : Fragment(R.layout.weather_result_fragment) {
                 view.findNavController().navigate(WeatherResultFragmentDirections.backTop())
             }.show(childFragmentManager, DeleteAreaDialogFragment::class.java.name)
         }
-        viewModel.updateWeather(args.areaName)
-        //読み込んだ結果を画面に表示する。
-        viewModel.weather.observe(viewLifecycleOwner) {
-            it?.let {
-                with(binding) {
-                    weatherType.text = it.description.firstOrNull()?.desc.orEmpty()
-                    val temp = it.main.getTemp()
-                    val feelTemp = it.main.getFeelTemp()
-                    val text = getString(R.string.temp_display, temp, feelTemp)
-                    tempText.text = text
+
+        viewModel.fetchWeather(args.areaName)
+
+        viewModel.status.observe(viewLifecycleOwner) {
+            setAllGone(binding)
+            when (val status = it) {
+                is Response.Loading -> {
+                    loadingBind(binding)
+                }
+                is Response.ErrorResponse -> {
+                    errorBind(binding, status.errorStatus)
+                }
+                is Response.SuccessResponse -> {
+                    successBind(binding, status.body)
                 }
             }
         }
+    }
+
+    private fun loadingBind(binding: WeatherResultFragmentBinding) {
+        binding.loadingView.visibility = View.VISIBLE
+    }
+
+    private fun errorBind(binding: WeatherResultFragmentBinding, errorStatus: ErrorStatus) {
+        when (errorStatus) {
+            is ErrorStatus.ErrorWithMessage -> {
+                binding.errorView.visibility = View.VISIBLE
+                binding.errorViewText.text = getString(errorStatus.messageId)
+            }
+            is ErrorStatus.UnAuthorizedErrorWithMessage -> {
+                binding.errorView.visibility = View.VISIBLE
+                binding.errorViewText.text = getString(errorStatus.messageId)
+            }
+            is ErrorStatus.AreaErrorWithMessage -> {
+                binding.areaNotFoundErrorView.visibility = View.VISIBLE
+                binding.areaNotFoundErrorText.text = getString(errorStatus.messageId)
+            }
+        }
+    }
+
+    private fun successBind(binding: WeatherResultFragmentBinding, weather: Weather) {
+        val temp = weather.main.getTemp()
+        val feelTemp = weather.main.getFeelTemp()
+        val text = getString(R.string.temp_display, temp, feelTemp)
+
+        binding.tempText.text = text
+        binding.weatherType.text = weather.description.firstOrNull()?.desc.orEmpty()
+        binding.resultView.visibility = View.VISIBLE
+    }
+
+    private fun setAllGone(binding: WeatherResultFragmentBinding) {
+        val gone = View.GONE
+        binding.resultView.visibility = gone
+        binding.errorView.visibility = gone
+        binding.loadingView.visibility = gone
+        binding.areaNotFoundErrorView.visibility = gone
     }
 }
