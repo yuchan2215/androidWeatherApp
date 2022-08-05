c1a2d1fed78db51d5cf21de07b3f235c98df5941
Miyayu <yuchan2215@gmail.com>
2022-08-05 17:31:49 +0900

    [add] 日本語で天気を取得できるように

--- app/src/main/java/xyz/miyayu/android/weatherapp/network/WeatherApiService.kt
+++ app/src/main/java/xyz/miyayu/android/weatherapp/network/WeatherApiService.kt
@@ -4,11 +4,21 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 import xyz.miyayu.android.weatherapp.network.json.Weather
+import java.util.*
 
 interface WeatherApiService {
     @GET("data/2.5/weather")
     suspend fun getWeather(
         @Query("appid") apiKey: String,
-        @Query("q") query: String
+        @Query("q") query: String,
+        @Query("lang") lang: String = getLang()
     ): Response<Weather>
+
+    companion object {
+        fun getLang(): String {
+            val isJpn = Locale.getDefault() == Locale.JAPAN
+            return if (isJpn) "ja" else "en"
+        }
+    }
 }
+
