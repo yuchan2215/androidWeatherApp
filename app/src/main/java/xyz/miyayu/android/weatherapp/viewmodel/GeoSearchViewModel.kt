3e7bcd186754f49fce303f58243e8a4fc8625996
Miyayu <yuchan2215@gmail.com>
2022-08-05 19:41:12 +0900

    [add] ViewModelの内容を作成

--- app/src/main/java/xyz/miyayu/android/weatherapp/viewmodel/GeoSearchViewModel.kt
+++ app/src/main/java/xyz/miyayu/android/weatherapp/viewmodel/GeoSearchViewModel.kt
@@ -1,7 +1,25 @@
 package xyz.miyayu.android.weatherapp.viewmodel
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import xyz.miyayu.android.weatherapp.repositories.GeoRepository
+import xyz.miyayu.android.weatherapp.utils.Response
 
 class GeoSearchViewModel : ViewModel() {
-    
+    private val _geoResponse: MutableLiveData<Response?> = MutableLiveData(null)
+    val geoResponse: LiveData<Response?> = _geoResponse
+
+    val isLoading = geoResponse.map {
+        return@map it is Response.Loading
+    }
+
+    fun fetchGeos(areaName: String) {
+        _geoResponse.value = Response.Loading
+        viewModelScope.launch(Dispatchers.Main) {
+            _geoResponse.value = GeoRepository.fetchGeos(areaName)
+        }
+    }
+
+
 }
