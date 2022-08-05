b8d6b6e3b7bc1ec2d9938e4a62650fedbc809122
Miyayu <yuchan2215@gmail.com>
2022-08-05 19:40:49 +0900

    [fix] factoryの修正

--- app/src/main/java/xyz/miyayu/android/weatherapp/viewmodel/factory/GeoSearchViewModelFactory.kt
+++ app/src/main/java/xyz/miyayu/android/weatherapp/viewmodel/factory/GeoSearchViewModelFactory.kt
@@ -3,13 +3,12 @@ package xyz.miyayu.android.weatherapp.viewmodel.factory
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import xyz.miyayu.android.weatherapp.viewmodel.GeoSearchViewModel
-import xyz.miyayu.android.weatherapp.viewmodel.WeatherViewModel
 
 class GeoSearchViewModelFactory(
 ) :
     ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(WeatherViewModel::class.java)) {
+        if (modelClass.isAssignableFrom(GeoSearchViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
             return GeoSearchViewModel() as T
         }
